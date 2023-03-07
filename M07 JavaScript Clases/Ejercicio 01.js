ed9b38c1e9897f191119dafec9d82ec14be56c86
Juan SB <juansb.dev@gmail.com>
2023-03-07 03:50:02 -0300

    sigue

--- M07 JavaScript Clases/Ejercicio 01.js
+++ M07 JavaScript Clases/Ejercicio 01.js
@@ -19,7 +19,7 @@ function crearUsuario() {
       }
       saludar() {
          return `Hola, mi nombre es ${this.nombre}`
-      }
+      }   
    }
    return Usuario;
 }
@@ -28,12 +28,24 @@ function agregarMetodoPrototype(Usuario) {
    // Agrega un método al prototipo de "Usuario".
    // El método debe llamarse "saludar" y debe devolver el string "Hello World!".
    // Tu código:
+   Usuario.prototype.saludar = function(){
+      return 'Hello World!'
+   }
+   return Usuario;
 }
 
 function agregarStringInvertida() {
    // Agrega un método al prototipo de "String" que devuelva la misma cadena de caracteres, pero invertida.
    // El método debe llamarse "reverse".
    // [PISTA]: necesitarás utilizar el objeto "this".
+   String.prototype.reverse = function(){
+      let newString = '';
+      for (var i = this.length - 1; i >= 0; i--) {
+            newString += this[i];      
+      }
+      return newString;
+
+   }
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
