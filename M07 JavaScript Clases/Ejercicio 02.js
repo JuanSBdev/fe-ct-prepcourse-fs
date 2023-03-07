17e34dd59dafeb454ec0427234191101964735d2
Juan SB <juansb.dev@gmail.com>
2023-03-07 13:19:40 -0300

    finalizado

--- M07 JavaScript Clases/Ejercicio 02.js
+++ M07 JavaScript Clases/Ejercicio 02.js
@@ -28,6 +28,8 @@ function crearInstanciaPersona(nombre, apellido, edad, domicilio) {
    // Recibirás las propiedades por parámetro.
    // Retornar la instancia creada.
    // Tu código:
+   let nP = new Persona (nombre, apellido, edad, domicilio);
+   return nP;
 }
 
 function agregarMetodo() {
@@ -35,6 +37,10 @@ function agregarMetodo() {
    // Este método toma la propiedad "nombre" y "edad", y devuelve el string:
    // Ejemplo: "Juan, 22 años".
    // Tu código:
+   Persona.prototype.datos =  function(){
+      return `${this.nombre}, ${this.edad} años`
+   }
+   return Persona;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
