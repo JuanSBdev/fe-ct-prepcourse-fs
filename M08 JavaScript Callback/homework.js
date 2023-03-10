0219c78a7c6b040fc9157a364ace345e5c2ee578
Juan SB <juansb.dev@gmail.com>
2023-03-10 19:58:49 -0300

    JS COMPPLETO

--- M08 JavaScript Callback/homework.js
+++ M08 JavaScript Callback/homework.js
@@ -61,6 +61,8 @@ function filter(arrayOfStrings) {
    // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
    // Luego retorna un nuevo arreglo con estos elementos.
    // Tu código:
+   return na = arrayOfStrings.filter(i => i[0]=== 'a');
+   
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
